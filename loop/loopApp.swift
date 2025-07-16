5ea92992b2c93e10fb1afc41b887897a72f183e0
andrewondara <a.ondara14@gmail.com>
2025-07-16 13:10:29 -0700

    Initial Commit

---
+++ loop/loopApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopApp.swift
+//  loop
+//
+//  Created by Andrew Ondara on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct loopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ loopTests/loopTests.swift
@@ -0,0 +1,17 @@
+//
+//  loopTests.swift
+//  loopTests
+//
+//  Created by Andrew Ondara on 7/16/25.
+//
+
+import Testing
+@testable import loop
+
+struct loopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
